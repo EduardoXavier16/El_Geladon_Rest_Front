a8fd5f066f7d382ed1211599f551edf77b5153f1
Eduardo Nascimento <edwardo.xavier@gmail.com>
2022-05-02 22:20:56 -0300

    Initial comit

---
+++ assets/script.js
@@ -0,0 +1,141 @@
+const baseURL = "http://localhost:3000/paletas";
+
+async function findAllPaletas() {
+  const response = await fetch(`${baseURL}/todas-paletas`);
+
+  const paletas = await response.json();
+  paletas.forEach((paleta) => {
+    document.getElementById("paletaList").insertAdjacentHTML(
+      "beforeend",
+      `<div class="PaletaListaItem" id="PaletaListaItem_${paleta.id}">
+            <div>
+                <div class="PaletaListaItem__sabor">${paleta.sabor}</div>
+                <div class="PaletaListaItem__preco">R$ ${paleta.preco.toFixed(
+                  2
+                )}</div>
+                <div class="PaletaListaItem__descricao">${
+                  paleta.descricao
+                }</div>
+                <div class= "PaletaListaItem__acoes">
+                  <button class= "Acoes__editar btn" onclick="abrirModal(${paleta.id})">Editar</button>
+                  <button class= "Acoes__apagar btn" onclick="">Apagar</button>
+                </div>
+            </div>
+                <img class="PaletaListaItem__foto" src=${
+                  paleta.foto
+                } alt=${`Paleta de ${paleta.sabor}`} />
+
+               
+            </div>`
+    );
+  });
+}
+
+findAllPaletas();
+
+async function findByIdPaleta() {
+  const id = document.querySelector("#idPaleta").value
+  const response = await fetch(`${baseURL}/paleta/${id}`);
+  const paleta = await response.json();
+  const paletaEscolhidaDiv = document.querySelector("#paletaEscolhida");
+  paletaEscolhidaDiv.innerHTML = 
+  `<div class="PaletaCardItem" id="PaletaListaItem_${paleta.id}">
+    <div>
+      <div class="PaletaCardItem__sabor">${paleta.sabor}</div>
+      <div class="PaletaCardItem__preco">R$ ${paleta.preco.toFixed(2)}</div>
+      <div class="PaletaCardItem__descricao">${paleta.descricao}</div>
+    </div>
+      <img class="PaletaCardItem__foto" src=${
+        paleta.foto
+      } alt=${`Paleta de ${paleta.sabor}`} />
+  </div>`;
+};
+
+async function abrirModal(id = null) {
+
+  if (id != null) {
+    document.querySelector('#title-header-modal').innerText = "Atualizar uma paleta"
+    document.querySelector('#button-form-modal').innerText = "Atualizar"
+
+    const response = await fetch(`${baseURL}/paleta/${id}`)
+    const paleta = await response.json();
+
+    document.querySelector("#sabor").value = paleta.sabor;
+    document.querySelector("#preco").value = paleta.preco;
+    document.querySelector("#descricao").value = paleta.descricao;
+    document.querySelector("#foto").value = paleta.foto;
+    document.querySelector("#foto").value = paleta.id;
+
+  }else{
+    document.querySelector('#title-header-modal').innerText = "Cadastrar uma paleta"
+  }
+  document.querySelector(".modal-overlay").style.display = "flex";
+}
+
+function fecharModalCadastro() {
+  document.querySelector(".modal-overlay").style.display = "none";
+
+  document.querySelector("#sabor").value = "";
+  document.querySelector("#preco").value = 0;
+  document.querySelector("#descricao").value = "";
+  document.querySelector("#foto").value = "";
+}
+
+async function createPaleta(){
+  const id = document.querySelector("#id").value;
+  const sabor = document.querySelector("#sabor").value;
+  const preco = document.querySelector("#preco").value;
+  const descricao = document.querySelector("#descricao").value;
+  const foto = document.querySelector("#foto").value;
+  
+
+  const paleta = {
+    id,
+    sabor, 
+    preco,
+    descricao,
+    foto,
+  };
+
+  const modoEdicaoAtivado = id > 0;
+
+  const endpoint = baseURL + (modoEdicaoAtivado ? `/update/${id}` : `/create`);
+
+  const response = await fetch(endpoint, {
+    method: modoEdicaoAtivado ? "put" : "post");
+    headers: {
+      "Content-Type" : "application/json",
+    },
+    node: 'cors',
+    body: json.stringify(paleta),
+  });
+
+  const novaPaleta = await response.json();
+
+  const html = 
+  `<div class="PaletaListaItem" id="PaletaListaItem_${paleta.id}">
+  <div>
+      <div class="PaletaListaItem__sabor">${novaPaleta.sabor}</div>
+      <div class="PaletaListaItem__preco">R$ ${novaPaleta.preco.toFixed(
+        2
+      )}</div>
+      <div class="PaletaListaItem__descricao">${
+        novaPaleta.descricao
+      }</div>
+  </div>
+      <img class="PaletaListaItem__foto" src=${
+        novaPaleta.foto
+      } alt=${`Paleta de ${novaPaleta.sabor}`} />
+  </div>`;
+
+  if(modoEdicaoAtivado){
+    document.querySelector(`PaletaListaItem_${id}`).outerHTML = html
+  }
+
+  document.querySelector("#paletaList"). insertAdjacentHTML("beforeend", html);
+
+  fecharModalCadastro();
+};
+
+
+
