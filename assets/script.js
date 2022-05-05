b1ef0bb59d4fbf7e662a4cf567a5c95a074ecf73
Eduardo Nascimento <edwardo.xavier@gmail.com>
2022-05-05 11:00:26 -0300

    Update of code

--- assets/script.js
+++ assets/script.js
@@ -47,18 +47,16 @@ findAllPaletas();
 async function findByIdPaleta() {
   const id = document.querySelector("#idPaleta").value
 
+  const response = await fetch(`${baseURL}/one-paleta/${id}`);
+  const paleta = await response.json();
+
   if(paletas.message != undefined) {
     localStorage.setItem('message', "Digite um ID para pesquisar!");
     localStorage.setItem('type', "danger");
-
-    msgAlert.innerText = localStorage.getItem("message");
-    msgAlert.classList.add(localStorage.getItem("type"));
-    closeMessageAlert();
+    showMessageAlert();
     return;
   };
 
-  const response = await fetch(`${baseURL}/one-paleta/${id}`);
-  const paleta = await response.json();
   const paletaEscolhidaDiv = document.querySelector("#paletaEscolhida");
   paletaEscolhidaDiv.innerHTML = 
   `<div class="PaletaCardItem" id="PaletaListaItem_${paleta._id}">
@@ -121,10 +119,11 @@ async function submitPaleta(){
 
   const modoEdicaoAtivado = id != "";
 
-  const endpoint = baseURL + (modoEdicaoAtivado ? `/update-paleta/${id}` : `/create-paleta`);
+  const endpoint = 
+    baseURL + (modoEdicaoAtivado ? `/update-paleta/${id}` : `/create-paleta`);
 
   const response = await fetch(endpoint, {
-    method: modoEdicaoAtivado ? "put" : "post");
+    method: modoEdicaoAtivado ? "put" : "post",
     headers: {
       "Content-Type" : "application/json",
     },
@@ -136,13 +135,38 @@ async function submitPaleta(){
 
   document.location.reload(true);
 
-  // if(modoEdicaoAtivado){
-  //   document.querySelector(`PaletaListaItem_${id}`).outerHTML = html
-  // }
+  if(novaPaleta.message != undefined) {
+    localStorage.setItem('message', novaPaleta.message);
+    localStorage.setItem('type', "danger");
+    showMessageAlert();
+    return;
+  };
 
-  // document.querySelector("#paletaList"). insertAdjacentHTML("beforeend", html);
-
+  if(modoEdicaoAtivado){
+    if(paletas.message != undefined) {
+      localStorage.setItem('message', "Psleta atualizada com sucesso!");
+      localStorage.setItem('type', "sucess");
+      return;
+    };
+  }
+  else {
+      localStorage.setItem('message', "Psleta criada com sucesso!");
+      localStorage.setItem('type', "sucess");
+  }
+  document.location.reload(true);    
   fecharModal();
+};
+
+function abrirModalDelete(id) {
+  document.querySelector("#overlay-delete").style.display = "flex";
+  const btnSim = document.querySelector(".btn_delete_yes");
+  btnSim.addEventListener("click", function() {
+    deletePaleta(id);
+  });
+}
+
+function fecharModalDelete() {
+  document.querySelector("#overlay-delete").style.display = "none";
 }
 
 async function deletePaleta (id) {
@@ -155,6 +179,8 @@ async function deletePaleta (id) {
   });
 
   const result = await response.json();
+  localStorage.setItem("message", result.message);
+  localStorage.setItem("type", "sucess");
   document.location.reload(true);
   fecharModalDelete();
 }
@@ -166,3 +192,11 @@ function closeMessageAlert(){
     localStorage.clear();
   }, 3000);
 };
+
+function showMessageAlert() {
+  msgAlert.innerText = localStorage.getItem("message");
+  msgAlert.classList.add(localStorage.getItem("type"));
+  closeMessageAlert();
+}
+
+showMessageAlert();
